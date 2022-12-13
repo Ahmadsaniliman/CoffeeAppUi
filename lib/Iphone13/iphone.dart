250e90fdd4e246e0b643d655cbc5caf323ed57d9
Ahmadsaniliman <saniahmadliman@gmail.com>
2022-12-13 22:09:15 +0100

    another commit

---
+++ lib/Iphone13/iphone.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class IphoneScreen extends StatelessWidget {
+  const IphoneScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(
+              'assets/images/background.png',
+            ),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Stack(
+          children: const [],
+        ),
+      ),
+    );
+  }
+}
